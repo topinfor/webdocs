b97ebda7faf0bf61012ab3fe6d0972368c54d58e
Michael Tavares <michael@topinfor.com.br>
2024-09-16 08:53:08 -0300

    Fist Comit

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from blog.views import home
+
+
+urlpatterns = [
+    path('', home),  # type: ignore
+]
---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# from django.urls import path
+
+# from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'blog/pages/home.html')

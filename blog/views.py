1067db4f50aaf5f7f008376c6501affb7c117f66
Michael Tavares <michael@topinfor.com.br>
2024-09-28 19:53:19 -0300

    Realizando inclusão de conteudo dinamico.

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
-# from django.urls import path
-
-# from django.http import HttpResponse
+from utils.blog.factory import make_post
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'blog/pages/home.html', context={
-        'name': 'Banco de Conhecimento'
+        'blogs': [make_post() for _ in range(10)],
     })
 
 
 def blog(request, id):
     return render(request, 'blog/pages/post_view.html', context={
-        'name': 'Banco de Conhecimento'
+        'blog': make_post(),
+        'is_detail_page': True,
     })
---
+++ utils/blog/factory.py
@@ -0,0 +1,35 @@
+from random import randint
+
+from faker import Faker
+
+fake = Faker('pt-BR')
+
+def rand_radio():
+    return randint (840, 900), randint(473, 573)
+
+
+
+def make_post():
+    return {
+        'post_number': randint (100, 900),
+        'title': fake.sentence(nb_words=4),
+        'description': fake.sentence(nb_words=40),
+        'post_tag': fake.sentence(nb_words=2),
+        'post_sector': fake.sentence(nb_words=2),
+        'post_content': fake.text(3000),
+        'created_at': fake.date_time(),
+        'author': {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+        },
+        'category': {
+            'name': fake.word(),
+            },
+        'cover': {
+            'url': 'https://loremflickr.com/%s/%s/network,internet' % rand_radio(),
+        }
+    }
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(make_post())
+    
